f1f5a4b6a2e8891c5c92b35d5832500a805422ef
Forrest Guice <forrestguice@users.noreply.github.com>
2021-12-10 21:04:25 -0700

    AlarmEventContract

--- suntimesaddon/src/main/java/com/forrestguice/suntimes/alarm/AlarmEventContract.java
+++ suntimesaddon/src/main/java/com/forrestguice/suntimes/alarm/AlarmEventContract.java
@@ -26,18 +26,29 @@ package com.forrestguice.suntimes.alarm;
  */
 public interface AlarmEventContract
 {
+    String AUTHORITY = "suntimeswidget.event.provider";
     String REQUIRED_PERMISSION = "suntimes.permission.READ_CALCULATOR";
 
-    String COLUMN_CONFIG_PROVIDER = "provider";         // String (provider reference)
-    String COLUMN_EVENT_NAME = "event_name";            // String (alarm/event ID)
-    String COLUMN_EVENT_TITLE = "event_title";          // String (display string)
-    String COLUMN_EVENT_SUMMARY = "event_summary";      // String (extended display string)
-    String COLUMN_EVENT_TIMEMILLIS = "event_time";      // long (timestamp millis)
+    String COLUMN_CONFIG_PROVIDER = "provider";                  // String (provider reference)
+    String COLUMN_EVENT_NAME = "event_name";                     // String (alarm/event ID)
+    String COLUMN_EVENT_TITLE = "event_title";                   // String (display string)
+    String COLUMN_EVENT_SUMMARY = "event_summary";               // String (extended display string)
+    String COLUMN_EVENT_SUPPORTS_REPEATING = "event_supports_repeat";        // int; REPEAT_SUPPORT_*; 0 (none), 1 (basic), 2 (daily)
+    String COLUMN_EVENT_SUPPORTS_OFFSETDAYS = "event_supports_offsetdays";   // String (boolean)
+    String COLUMN_EVENT_REQUIRES_LOCATION = "event_requires_location";       // String (boolean)
+
+    int REPEAT_SUPPORT_NONE = 0;
+    int REPEAT_SUPPORT_BASIC = 1;
+    int REPEAT_SUPPORT_DAILY = 2;
 
     String QUERY_EVENT_INFO = "eventInfo";
     String[] QUERY_EVENT_INFO_PROJECTION = new String[] {
-            COLUMN_EVENT_NAME, COLUMN_EVENT_TITLE, COLUMN_EVENT_SUMMARY
+            COLUMN_EVENT_NAME, COLUMN_EVENT_TITLE, COLUMN_EVENT_SUMMARY,
+            COLUMN_EVENT_SUPPORTS_REPEATING, COLUMN_EVENT_SUPPORTS_OFFSETDAYS, COLUMN_EVENT_REQUIRES_LOCATION
     };
+
+    String COLUMN_EVENT_TIMEMILLIS = "event_time";      // long (timestamp millis)
+
     String QUERY_EVENT_CALC = "eventCalc";
     String[] QUERY_EVENT_CALC_PROJECTION = new String[] {
             COLUMN_EVENT_NAME, COLUMN_EVENT_TIMEMILLIS
