7141466476f11fbafd5b0a570964960b315e14bd
Forrest Guice <forrestguice@users.noreply.github.com>
2023-08-04 12:41:13 -0700

    test

--- suntimesaddon/src/androidTest/java/com/forrestguice/suntimes/addon/SuntimesUtilsTest.java
+++ suntimesaddon/src/androidTest/java/com/forrestguice/suntimes/addon/SuntimesUtilsTest.java
@@ -21,7 +21,7 @@ package com.forrestguice.suntimes.addon;
 import android.content.Context;
 import android.support.test.InstrumentationRegistry;
 import android.support.test.runner.AndroidJUnit4;
-import android.test.RenamingDelegatingContext;
+//import android.test.RenamingDelegatingContext;
 
 import com.forrestguice.suntimes.addon.ui.SuntimesUtils;
 
@@ -48,7 +48,8 @@ public class SuntimesUtilsTest
     @Before
     public void setup()
     {
-        mockContext = new RenamingDelegatingContext(InstrumentationRegistry.getTargetContext(), "test_");
+        //mockContext = new RenamingDelegatingContext(InstrumentationRegistry.getTargetContext(), "test_");
+        mockContext = InstrumentationRegistry.getTargetContext();
         SuntimesUtils.initDisplayStrings(mockContext);
         utils = new SuntimesUtils();
     }
